ae31789436042acab19d994a5dcf246d7e2b2b2d
takashi oka <takashi.oka@gmail.com>
2021-04-23 15:57:08 +0900

    function update

---
+++ db/migrate/20210422151241_create_stations.rb
@@ -0,0 +1,10 @@
+class CreateStations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stations do |t|
+      t.string :line_name
+      t.string :station_name
+      t.integer :walking_time
+      t.references :estate, foreign_key: true
+    end
+  end
+end
